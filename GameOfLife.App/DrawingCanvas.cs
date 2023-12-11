82d616eced4c6ed9aa8e057660c00c643a37d8b1
electricessence <5899455+electricessence@users.noreply.github.com>
2023-12-11 13:13:38 -0800

    Cleanup.

--- GameOfLife.App/DrawingCanvas.cs
+++ GameOfLife.App/DrawingCanvas.cs
@@ -13,136 +13,135 @@ namespace GameOfLife.App;
 
 public class DrawingCanvas : Control
 {
-	private const int BoxSize = 7;
-	private const int BoxSizePlus = BoxSize + 2;
-	private const int WidthInBoxes = 160;
-	private const int HeightInBoxes = 80;
-	private const int Delay = 100;
-	private const int ScrambleDensity = 3;
+    private const int BoxSize = 7;
+    private const int BoxSizePlus = BoxSize + 2;
+    private const int WidthInBoxes = 160;
+    private const int HeightInBoxes = 80;
+    private const int Delay = 100;
+    private const int ScrambleDensity = 3;
 
-	private static readonly SolidColorBrush Background = new(Color.FromArgb(255, 16, 16, 16));
-	private static readonly SolidColorBrush Foreground = new(Color.FromArgb(255, 200, 200, 200));
-	private static readonly Pen ForegroundPen = new(Foreground, 1);
-	private readonly DispatcherTimer _timer;
-	private Grid grid;
-	private Grid grid2;
-	Task _next;
+    private static readonly SolidColorBrush Background = new(Color.FromArgb(255, 16, 16, 16));
+    private static readonly SolidColorBrush Foreground = new(Color.FromArgb(255, 200, 200, 200));
+    private readonly DispatcherTimer _timer;
+    private Grid _grid;
+    private Grid _grid2;
+    Task _next;
 
-	readonly ConcurrentQueue<Point2D> _pointQueue = new();
+    readonly ConcurrentQueue<Point2D> _pointQueue = new();
 
-	readonly ConcurrentQueue<List<Rect>> _pool = new();
-	readonly ConcurrentQueue<List<Rect>> _rectQueue = new();
+    readonly ConcurrentQueue<List<Rect>> _pool = new();
+    readonly ConcurrentQueue<List<Rect>> _rectQueue = new();
 
-	public DrawingCanvas()
-	{
-		var bounds = new Bounds(WidthInBoxes, HeightInBoxes);
+    public DrawingCanvas()
+    {
+        var bounds = new Bounds(WidthInBoxes, HeightInBoxes);
 
-		grid = new Grid(bounds);
-		grid2 = new Grid(bounds);
+        _grid = new Grid(bounds);
+        _grid2 = new Grid(bounds);
 
-		_timer = new DispatcherTimer
-		{
-			Interval = TimeSpan.FromMilliseconds(Delay)
-		};
+        _timer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromMilliseconds(Delay)
+        };
 
-		_timer.Tick += UpdateDrawing;
+        _timer.Tick += UpdateDrawing;
 
-		_next = grid.ScrambleRadialSymmetricAsync(ScrambleDensity)
-			.ContinueWith(
-				_ => _timer.Start(),
-				TaskContinuationOptions.OnlyOnRanToCompletion);
+        _next = _grid.ScrambleRadialSymmetricAsync(ScrambleDensity)
+            .ContinueWith(
+                _ => _timer.Start(),
+                TaskContinuationOptions.OnlyOnRanToCompletion);
 
-		this.PointerPressed += PointerPressedHandler;
-		this.PointerReleased += PointerReleasedHandler;
-		this.PointerMoved += PointerMovedHandler;
-	}
+        this.PointerPressed += PointerPressedHandler;
+        this.PointerReleased += PointerReleasedHandler;
+        this.PointerMoved += PointerMovedHandler;
+    }
 
-	private void UpdateDrawing(object? sender, EventArgs e)
-	{
-		_next.Wait();
+    private void UpdateDrawing(object? sender, EventArgs e)
+    {
+        _next.Wait();
 
-		while (_pointQueue.TryDequeue(out var p))
-		{
-			var c = grid.GetCell(p);
-			c.SetValueIfInBounds(true);
-			c.GetOffset(0, 1).SetValueIfInBounds(true);
-			c.GetOffset(1, 0).SetValueIfInBounds(true);
-			c.GetOffset(-1, 0).SetValueIfInBounds(true);
-			c.GetOffset(0, -1).SetValueIfInBounds(true);
-		}
+        while (_pointQueue.TryDequeue(out var p))
+        {
+            var c = _grid.GetCell(p);
+            c.SetValueIfInBounds(true);
+            c.GetOffset(0, 1).SetValueIfInBounds(true);
+            c.GetOffset(1, 0).SetValueIfInBounds(true);
+            c.GetOffset(-1, 0).SetValueIfInBounds(true);
+            c.GetOffset(0, -1).SetValueIfInBounds(true);
+        }
 
-		List<Rect> rects = _pool.TryDequeue(out var r) ? r : [];
-		foreach (var row in grid.Rows)
-		{
-			foreach (var cell in row)
-			{
-				var (x, y) = cell.Location;
-				if (cell)
-				{
-					var rect = new Rect(x * BoxSize - 1, y * BoxSize - 1, BoxSizePlus, BoxSizePlus);
-					rects.Add(rect);
-					//context.FillRectangle(Foreground, rect);
-				}
-			}
-		}
-		_rectQueue.Enqueue(rects);
+        List<Rect> rects = _pool.TryDequeue(out var r) ? r : [];
+        foreach (var row in _grid.Rows)
+        {
+            foreach (var cell in row)
+            {
+                var (x, y) = cell.Location;
+                if (cell)
+                {
+                    var rect = new Rect(x * BoxSize - 1, y * BoxSize - 1, BoxSizePlus, BoxSizePlus);
+                    rects.Add(rect);
+                    //context.FillRectangle(Foreground, rect);
+                }
+            }
+        }
+        _rectQueue.Enqueue(rects);
 
-		_next = grid.NextAsync(grid2)
-			.ContinueWith(
-				_ => (grid2, grid) = (grid, grid2),
-				TaskContinuationOptions.OnlyOnRanToCompletion);
+        _next = _grid.NextAsync(_grid2)
+            .ContinueWith(
+                _ => (_grid2, _grid) = (_grid, _grid2),
+                TaskContinuationOptions.OnlyOnRanToCompletion);
 
-		InvalidateVisual(); // Invalidate to redraw
-	}
+        InvalidateVisual(); // Invalidate to redraw
+    }
 
-	public override void Render(DrawingContext context)
-	{
-		base.Render(context);
+    public override void Render(DrawingContext context)
+    {
+        base.Render(context);
 
-		// Give the draw phase max CPU.
-		Draw(context);
-	}
+        // Give the draw phase max CPU.
+        Draw(context);
+    }
 
-	private void Draw(DrawingContext context)
-	{
-		List<Rect>? rects = null;
-		while (_rectQueue.TryDequeue(out var r)) rects = r; // Skip any missed frames.
-		if (rects is null) return;
+    private void Draw(DrawingContext context)
+    {
+        List<Rect>? rects = null;
+        while (_rectQueue.TryDequeue(out var r)) rects = r; // Skip any missed frames.
+        if (rects is null) return;
 
-		context.FillRectangle(Background, new Rect(0, 0, WidthInBoxes * BoxSize, HeightInBoxes * BoxSize));
-		foreach (var rect in rects)
+        context.FillRectangle(Background, new Rect(0, 0, WidthInBoxes * BoxSize, HeightInBoxes * BoxSize));
+        foreach (var rect in rects)
         {
             context.FillRectangle(Foreground, rect, 2);
         }
 
         rects.Clear();
-		_pool.Enqueue(rects);
-	}
+        _pool.Enqueue(rects);
+    }
 
-	private int _pressed;
+    private int _pressed;
 
-	private void AddPointerEvent(object? sender, PointerEventArgs e)
-	{
-		var point = e.GetCurrentPoint(sender as Control);
-		int x = (int)(point.Position.X / BoxSize);
-		int y = (int)(point.Position.Y / BoxSize);
-		_pointQueue.Enqueue(new(x, y));
-	}
+    private void AddPointerEvent(object? sender, PointerEventArgs e)
+    {
+        var point = e.GetCurrentPoint(sender as Control);
+        int x = (int)(point.Position.X / BoxSize);
+        int y = (int)(point.Position.Y / BoxSize);
+        _pointQueue.Enqueue(new(x, y));
+    }
 
-	private void PointerPressedHandler(object? sender, PointerPressedEventArgs e)
-	{
-		Interlocked.CompareExchange(ref _pressed, 1, 0);
-		AddPointerEvent(sender, e);
-	}
+    private void PointerPressedHandler(object? sender, PointerPressedEventArgs e)
+    {
+        Interlocked.CompareExchange(ref _pressed, 1, 0);
+        AddPointerEvent(sender, e);
+    }
 
-	private void PointerMovedHandler(object? sender, PointerEventArgs e)
-	{
-		if (_pressed == 1) AddPointerEvent(sender, e);
-	}
+    private void PointerMovedHandler(object? sender, PointerEventArgs e)
+    {
+        if (_pressed == 1) AddPointerEvent(sender, e);
+    }
 
-	private void PointerReleasedHandler(object? sender, PointerReleasedEventArgs e)
-	{
-		Interlocked.CompareExchange(ref _pressed, 0, 1);
-		AddPointerEvent(sender, e);
-	}
+    private void PointerReleasedHandler(object? sender, PointerReleasedEventArgs e)
+    {
+        Interlocked.CompareExchange(ref _pressed, 0, 1);
+        AddPointerEvent(sender, e);
+    }
 }
